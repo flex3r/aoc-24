85a1d4045513156d808e2464f66eccbbf55c1846
Felix <felixschmidbb@gmail.com>
2024-12-01 20:00:24 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,34 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = parseLocations(input)
+        return left.zip(right).sumOf { abs(it.first - it.second) }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (left, right) = parseLocations(input)
+        val counted = right.groupingBy { it }.eachCount()
+        return left.sumOf { a -> a * counted.getOrDefault(a, 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun parseLocations(input: List<String>): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    input.forEach { line ->
+        val (first, second) = line.split("   ")
+        left += first.toInt()
+        right += second.toInt()
+    }
+    return left.sorted() to right.sorted()
 }
